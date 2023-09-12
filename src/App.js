9667b0e05d2440a7a1f1a69086a4bef0448a67c2
ProgasiFox <96259001+ProgasiFox@users.noreply.github.com>
2023-09-12 21:29:47 +0300

    Created to fix BODY.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css';
 import Header from "./containers/Header.jsx";
+import Body from "./containers/Body.jsx"
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
-    </>
+      <Body />
+    </div>
   );
 }
 
---
+++ src/containers/Body.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+import "./Body.css";
+
+export default function Body() {
+  return (
+    <main>
+      <ul className="main-ul">
+        <li>
+          <h1>Health Articles and Information</h1>
+          <ul>
+            <li>
+              <h2>Nutrition</h2>
+            </li>
+            <li>
+              <h2>Exercise</h2>
+            </li>
+            <li>
+              <h2>Mental Health</h2>
+            </li>
+            <li>
+              <h2>Medical Conditions</h2>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Trusted Sources</h1>
+          <ul>
+            <li>
+              <h2>World Health Organization (WHO)</h2>
+            </li>
+            <li>
+              <h2>CDC (Centers for Disease Control and Prevention)</h2>
+            </li>
+            <li>
+              <h2>NIH (National Institutes of Health)</h2>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Healthy Living Tips</h1>
+          <ul>
+            <li>
+              <h2>Diet Recommendations</h2>
+            </li>
+            <li>
+              <h2>Fitness Routines</h2>
+            </li>
+            <li>
+              <h2>Stress Management</h2>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Preventive Care Guidelines</h1>
+          <ul>
+            <li>
+              <h2>Immunizations</h2>
+            </li>
+            <li>
+              <h2>Screenings</h2>
+            </li>
+            <li>
+              <h2>Check-Up Schedule</h2>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Symptom Checker</h1>
+        </li>
+        <li>
+          <h1>Local Healthcare Services</h1>
+          <ul>
+            <li>
+              <h2>Healthcare Providers</h2>
+              <ul>
+                <li>
+                  <h2>Clinics</h2>
+                </li>
+                <li>
+                  <h2>Hospitals</h2>
+                </li>
+                <li>
+                  <h2>Pharmacies</h2>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Emergency Contacts</h1>
+          <ul>
+            <li>
+              <h2>911</h2>
+            </li>
+            <li>
+              <h2>Local Emergency Services</h2>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Mental Health Resources</h1>
+          <ul>
+            <li>
+              <h2>Crisis Hotlines</h2>
+            </li>
+            <li>
+              <h2>Therapy Services</h2>
+            </li>
+            <li>
+              <h2>Self-Help Resources</h2>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Healthy Recipes</h1>
+        </li>
+        <li>
+          <h2>Fitness and Exercise Resources</h2>
+          <ul>
+            <li>
+              <h2>Workout Routines</h2>
+            </li>
+            <li>
+              <h2>Exercise Videos</h2>
+            </li>
+            <li>
+              <h2>Fitness Tips</h2>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Medication Information</h1>
+          <ul>
+            <li>
+              <h2>Common Medications</h2>
+              <ul>
+                <li>
+                  <h2>Side Effects</h2>
+                </li>
+                <li>
+                  <h2>Dosage Instructions</h2>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h1>Healthcare News and Updates</h1>
+        </li>
+        <li>
+          <h2>Support Groups and Forums</h2>
+        </li>
+        <li>
+          <h2>Health Apps and Tools</h2>
+        </li>
+        <li>
+          <h2>Legal and Ethical Information</h2>
+        </li>
+        <li>
+          <h2>Multilingual Resources</h2>
+        </li>
+        <li>
+          <h2>References and Citations</h2>
+        </li>
+      </ul>
+    </main>
+  );
+}
